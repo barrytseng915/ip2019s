b3edd3d5968876aae03f81af36e5a368180879a9
barrytseng915 <52521469+barrytseng915@users.noreply.github.com>
2019-07-08 17:14:36 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,15 +4,44 @@
 
 
 function start(e) {
+    var name = [
+        {
+            char: '曾',
+            big5: 'B4BF',
+            cns:  '1-5F64',
+            unicode: '66FE'
+        },
+        {
+            char: '勻',
+            big5: 'A4C3',
+            cns:  '1-4524',
+            unicode: '52FB'
+        },
+        {
+            char: '搴',
+            big5: 'BA53',
+            cns:  '1-687A',
+            unicode: '6434'
+        }
+    ];
 
-}
+    var ele = document.getElementById("div123");
 
-/*
-https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+    console.log(ele);
 
-document.addEventListener(event, function, useCapture)
+    for (var i = 0; i < 3; i++) {
 
-true - The event handler is executed in the capturing phase
-false- Default. The event handler is executed in the bubbling phase
-*/
-window.addEventListener( "load", start, false );
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+        console.log('name = ' + name[i].char);
+
+    }
+};
+
+window.addEventListener("load", start , false);
